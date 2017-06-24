71a35b2f69226cdd6dc0caffcbdea77131110a57
David <davidkwon@Davids-MacBook-Pro.local>
2017-06-24 17:10:44 -0400

    added a toggle

--- weather.js
+++ weather.js
@@ -4,16 +4,15 @@ $(document).ready(function(){
 		getWeather();
 	});
 
-
-
-
-
 	$("#city").on("keypress", function(e) {
 		if (e.keyCode == 13) {
 			getWeather();
 			return false;
 		}
-	})
+	});
+
+
+
 
 });
 
@@ -23,35 +22,49 @@ function getWeather() {
 
 	var city = $("#city").val()
 	if (city === "") {
-		alert("Invalid city name");
+		alert("Please enter a city name.");
 	}else {
+		fadeOutForm();
 		getWeatherJson(city);
+
 	}
 	
-	
-
 
 }
 
 function getWeatherJson(city) {
 
 	$.ajax({
-		url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=9163b6aabce8d204588292197d456a76",
+		url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&APPID=9163b6aabce8d204588292197d456a76",
 		success: function(data) {
-			printData(data);
+			outputData(data);
 		}
 	});
 
 }
 
-function printData(data) {
+function outputData(data) {
+
+
 	var desc = data.weather[0].description;
+	desc = desc.charAt(0).toUpperCase() + desc.substring(1);
 	var cityName = data.name;
 	var countryName = data.sys.country;
 	var degrees = data.main.temp;
 
-	console.log(cityName);
-	console.log(countryName);
-	console.log(desc);
-	console.log(degrees);
+	$(".main").append('<div class="data-div"></div>');
+	$(".main").css("height", "30rem");
+	$(".data-div").append("<p class='data'>Location: "+ cityName +", "+ countryName+"</p>");
+	$(".data-div").append("<p class='data'>Temperature: "+ degrees +"</p>");
+	$(".data-div").append("<p class='data'>"+ desc +"</p>");
 }
+
+
+/**
+ * Fades out .form-div
+ * @return {None}
+ */
+function fadeOutForm() {
+	$(".form-div").fadeOut("slow");
+}
+
